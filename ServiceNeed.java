4677e576947b7cf6a00905fdb3db454c508bdd9d
GraysonCordie <77179585+GraysonCordie@users.noreply.github.com>
2021-02-21 20:23:16 -0800

    Add files via upload

---
+++ ServiceNeed.java
@@ -0,0 +1,17 @@
+package edu.cgcc.cs161;
+
+public class ServiceNeed {
+	
+	private int servicecodes;
+	
+	public ServiceNeed identifyNeed(Survey a) {
+		
+		//Identify need from survey
+		
+		ServiceNeed b;
+		
+		return b; //Return indentified needs
+		
+	}
+
+}
---
+++ Survey.java
@@ -0,0 +1,15 @@
+package edu.cgcc.cs161;
+
+public class Survey {
+	
+	public Survey fillOutSurvey() {
+		
+		//Ask survey questions
+		
+		Survey results;
+		
+		return results; //Return answers
+		
+	}
+
+}
