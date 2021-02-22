578632e1d43a4d876c1c6b5d73662adbeb18ed47
GraysonCordie <77179585+GraysonCordie@users.noreply.github.com>
2021-02-21 20:25:23 -0800

    Add files via upload

---
+++ ClinicService.java
@@ -0,0 +1,20 @@
+package edu.cgcc.cs161;
+
+public class ClinicService {
+	
+	public ClinicService getAvailServices() {
+		
+		ClinicService a;
+		
+		return a; //Display services
+		
+	}
+	
+	public ServiceNeed compare(ServiceNeed a) {
+		
+		ServiceNeed b = a; //Compare need to services
+		
+		return b;
+	}
+
+}
